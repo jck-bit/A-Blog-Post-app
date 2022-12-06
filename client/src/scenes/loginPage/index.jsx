cf7ced4a2c6a9284b6eab66c50aa02ad60990ba8
jck-bit <jckkinyanjui@gmail.com>
2022-12-06 16:08:11 +0300

    created the login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,31 @@
+import {
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typography,
+    useTheme,
+  } from "@mui/material";
+
+
 const Login = () =>{
-    return (<div>Login Page</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+
+    return (
+    <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography 
+            fontWeight="bold"
+            fontSize="320px"
+            color="primary"
+            >
+                sociopidia
+            </Typography>
+        </Box>
+        <Box></Box>
+    </Box>
+    )
 }
 
 export default Login
