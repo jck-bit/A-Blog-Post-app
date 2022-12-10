7bd60f30a42fa7781c651c1ae3867169dfe88bad
jck-bit <jckkinyanjui@gmail.com>
2022-12-10 20:25:17 +0300

    Edited the Widget Wrapper

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -4,8 +4,8 @@ import { styled } from "@mui/system";
 
 const WidgetWrapper = styled(Box) (({theme}) => ({
     padding: "1.5rem 1.5rem  0.75rem 1.5rem",
-    backgroundColour: theme.palette.background.alt,
-    borderRadius: "0.75 rem"
+    backgroundColor: theme.palette.background.alt,
+    borderRadius: "0.75rem"
 }))
 
 export default WidgetWrapper;
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -32,6 +32,7 @@ export const authSlice = createSlice({
         setPosts:(state, action) =>{
             state.posts = action.payload.posts;
         },
+        
         setPost:(state, action) =>{
             const updatedPosts = state.posts.map((post) =>{
                 if(post._id === action.payload.post_id) return action.payload.post;
